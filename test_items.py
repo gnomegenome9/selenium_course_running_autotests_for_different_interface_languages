a93c6d7c80858a9bdf1d311709289f342148e4c8
gnomegenome9 <125520515+gnomegenome9@users.noreply.github.com>
2025-11-12 21:16:12 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,11 +1,15 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
 def test_product_page_contains_button_to_add_to_cart(browser):
     browser.get(link)
-    browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
-    
+
+    time.sleep(30)
+
+    button = browser.find_elements(By.CLASS_NAME, "btn-add-to-basket")
+    assert len(button) > 0, "Страница товара не содержит кнопку добавления в корзину"
